87ca1590ba2af323e914e2761f554197c09b6055
Novo <120156959+Novo1999@users.noreply.github.com>
2023-07-29 21:01:56 +0600

    can play

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,10 @@ const initialState = {
   difficulty: null,
   images: [],
   status: "",
+  clickedArr: [],
+  clicked: "",
+  score: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -23,19 +27,41 @@ function reducer(state, action) {
         status: "loaded",
         images: action.payload,
       };
+    case "clickedImage":
+      if (state.clickedArr.includes(action.payload))
+        return { ...state, clickedArr: [], score: 0, highscore: state.score };
+      return {
+        ...state,
+        clicked: action.payload,
+        clickedArr: [...state.clickedArr, action.payload],
+        status: state.clickedArr.includes(action.payload) ? "lose" : "continue",
+        score: state.score + 1,
+      };
+
+    default:
+      throw new Error("Unknown");
   }
 }
 
 function App() {
-  const [{ images, difficulty, status }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [
+    { images, score, difficulty, status, clicked, clickedArr, highscore },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
+  console.log(clicked);
+  console.log(clickedArr);
+  console.log(status);
   return (
     <div>
       {/* <StartScreen state={state} dispatch={dispatch} /> */}
-      <Game dispatch={dispatch} images={images} />
+      <Game
+        dispatch={dispatch}
+        score={score}
+        images={images}
+        highscore={highscore}
+        status={status}
+      />
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 
-function Game({ images, dispatch }) {
+function Game({ images, dispatch, score, highscore, status }) {
   const [gameImages, setGameImages] = useState(images);
+  const [shuffled, setShuffled] = useState(false);
+
   useEffect(() => setGameImages(images), [images]);
+
+  // Image shuffling algorithm
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -21,6 +25,12 @@ function Game({ images, dispatch }) {
   const randomIndexList = getRandomIndexList(arrayLength);
   const shuffledImages = randomIndexList.map((index) => images[index]);
 
+  function handleImageClick(img) {
+    setTimeout(() => setGameImages(shuffledImages), 350);
+    setShuffled((status) => !status);
+    console.log(img.id);
+    dispatch({ type: "clickedImage", payload: img.id });
+  }
   useEffect(() => {
     async function fetchWaifu() {
       try {
@@ -29,6 +39,11 @@ function Game({ images, dispatch }) {
         );
         const data = await res.json();
         dispatch({ type: "getImages", payload: data.data });
+        data.data.map((item) => {
+          item.id = `id-${Math.floor(
+            Math.random() * Math.floor(Math.random() * Date.now())
+          )}`;
+        });
         console.log(data);
       } catch (err) {
         console.log(err);
@@ -37,17 +52,23 @@ function Game({ images, dispatch }) {
     fetchWaifu();
   }, [dispatch]);
   return (
-    <div className="game">
-      {gameImages.map((img, i) => (
-        <img
-          onClick={() => setGameImages(shuffledImages)}
-          className="game-image"
-          key={i}
-          src={img.jpg.image_url}
-          alt="img"
-        ></img>
-      ))}
-    </div>
+    <>
+      <div className="scores">
+        <h1 className="score">Score: {score}</h1>
+        <h1 className="highscore">HighScore: {highscore}</h1>
+      </div>
+      <div className="game">
+        {gameImages.map((img, i) => (
+          <img
+            onClick={() => handleImageClick(img)}
+            className={`game-image ${shuffled ? "animation" : "animation-2"}`}
+            key={i}
+            src={img.jpg.image_url}
+            alt="img"
+          ></img>
+        ))}
+      </div>
+    </>
   );
 }
 
