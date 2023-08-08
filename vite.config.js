2e3cea336e527b473c8965115924c1b10f1ed727
Novodip Mondal <neptune.navo@gmail.com>
2023-08-08 17:34:05 +0600

    rename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/memory-game-react.js",
+  base: "/Memory-Game-React",
 });
